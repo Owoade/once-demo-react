f76b143e55d450b5250d27c7f047ce06f793d913
Anuoluwapo <63651250+Owoade@users.noreply.github.com>
2023-06-22 05:08:01 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Once from "checkout-once";
 import {
   ChakraProvider,
   Box,
@@ -8,31 +9,80 @@ import {
   Code,
   Grid,
   theme,
+  Heading,
+  UnorderedList,
+  ListItem,
+  Button,
+  List,
+  Image,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+const items = [
+  {
+    id: 1,
+    name: 'Samsung Laptop',
+    price: 200000,
+    _price: "200,000",
+    image: 'https://solaroidenergy.com/wp-content/uploads/2023/03/can-i-replace-laptop-with-ipad-pro.jpg',
+  },
+  {
+    id: 2,
+    name: 'Iphone XR',
+    price: 100000,
+    _price: "100,000",
+    image: 'https://img.freepik.com/free-vector/realistic-display-smartphone-with-different-apps_52683-30241.jpg',
+  },
+  {
+    id: 3,
+    name: 'Headphones',
+    price: 50000,
+    _price: "50,000",
+    image: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6373/6373460_sd.jpg',
+  },
+];
+
+const totalAmount = items.reduce((sum, item) => sum + item.price, 0);
+
+export const App = () => {
+  const [ loading, setLoading ] = React.useState(false);
+
+  function handleSubmit(){
+
+    setLoading(true);
+
+    const once = new Once({
+      amount: 35000000,
+      successCallback:() => {
+        setLoading(false);
+        window.alert("Transaction Successful");
+      }
+    })
+
+    once.checkout()
+
+  }
+  return( <ChakraProvider theme={theme}>
+    <Box p={4} maxW="400px" mx="auto">
+        <Heading as="h1" size="xl" mb={6}>
+          Cart Page
+        </Heading>
+        <List spacing={4} mb={6}>
+          {items.map((item) => (
+            <ListItem key={item.id} fontSize="lg" display="flex" alignItems="center">
+              <Image src={item.image} alt={item.name} boxSize="50px" mr={4} />
+              <Box>{item.name} - ₦{item._price}</Box>
+            </ListItem>
+          ))}
+        </List>
+        <Box textAlign="right" fontWeight="bold" fontSize="xl" mb={6}>
+          Total: ₦350,000
+        </Box>
+        <Button onClick={handleSubmit} isLoading={loading} colorScheme="teal" size="lg" w="100%">
+          Checkout
+        </Button>
+      </Box>
+    </ChakraProvider>)
+ 
+}
